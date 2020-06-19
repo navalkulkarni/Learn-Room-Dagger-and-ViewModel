f6012d9041b52ad8eadc1cde5db66d33e6771e87
Naval Kulkarni <navalkulkarni@gmail.com>
2020-06-19 08:52:59 +0530

    add getAllAddresses and deleteAddress method and insert dummy addresses in MainViewModel.kt

--- app/src/main/java/com/mindorks/bootcamp/learndagger/ui/main/MainViewModel.kt
+++ app/src/main/java/com/mindorks/bootcamp/learndagger/ui/main/MainViewModel.kt
@@ -21,7 +21,7 @@ class MainViewModel @Inject constructor(
         private val databaseService: DatabaseService,
         private val networkService: NetworkService) {
         private var allUsers: List<User> = emptyList()
-
+        private var allAddresses : List<Address> = emptyList()
     companion object{
         const val TAG = "MainViewModel"
     }
@@ -30,19 +30,32 @@ class MainViewModel @Inject constructor(
 
     val userList = MutableLiveData<List<User>>()
 
+    val addressList = MutableLiveData<List<Address>>()
+
     init {
         compositeDisposable.add(
                 databaseService.getUserDao().count().
                         flatMap {
                             if(it == 0)
-                                databaseService.getUserDao().insertMany(
-                                        User(name = "Test 1",address = Address("San Diego",country = "USA")),
-                                        User(name = "Test 2",address = Address("San Diego",country = "USA")),
-                                        User(name = "Test 3",address = Address("San Diego",country = "USA")),
-                                        User(name = "Test 4",address = Address("San Diego",country = "USA")),
-                                        User(name = "Test 5",address = Address("San Diego",country = "USA")),
-                                        User(name = "Test 6",address = Address("San Diego",country = "USA"))
+                                databaseService.getAddressDao().insertMany(
+                                        Address(city = "San Diego",country = "USA"),
+                                        Address(city = "San Diego",country = "USA"),
+                                        Address(city = "San Diego",country = "USA"),
+                                        Address(city = "San Diego",country = "USA"),
+                                        Address(city = "San Diego",country = "USA"),
+                                        Address(city = "San Diego",country = "USA")
                                 )
+                                        .flatMap {addressIds->
+                                            databaseService.getUserDao().insertMany(
+                                                    User(name = "Test 1",addressId = addressIds[0]),
+                                                    User(name = "Test 2",addressId = addressIds[1] ),
+                                                    User(name = "Test 3",addressId = addressIds[2]),
+                                                    User(name = "Test 4",addressId = addressIds[3]),
+                                                    User(name = "Test 5",addressId = addressIds[4]),
+                                                    User(name = "Test 6",addressId = addressIds[5])
+                                            )
+                                        }
+
                             else Single.just(0)
                         }
                         .subscribeOn(Schedulers.io())
@@ -89,6 +102,45 @@ class MainViewModel @Inject constructor(
                                     {
                                         allUsers = it
                                         userList.postValue(it)
+                                    },
+                                    {
+                                        Log.d(TAG, it.toString())
+                                    }
+                            )
+            )
+    }
+
+    fun getAllAddresses(){
+        compositeDisposable.add(
+                databaseService.getAddressDao()
+                        .getAllAddresses()
+                        .subscribeOn(Schedulers.io())
+                        .subscribe(
+                                {
+                                    allAddresses = it
+                                    Log.d(TAG,allAddresses.toString())
+                                    addressList.postValue(it)
+                                },
+                                {
+                                    Log.d(TAG, it.toString())
+                                }
+                        )
+        )
+    }
+
+    fun deleteAddress(){
+        if(allAddresses.isNotEmpty())
+            compositeDisposable.add(
+                    databaseService.getAddressDao()
+                            .deleteAddress(allAddresses[0])
+                            .flatMap {
+                                databaseService.getAddressDao().getAllAddresses()
+                            }
+                            .subscribeOn(Schedulers.io())
+                            .subscribe(
+                                    {
+                                        allAddresses = it
+                                        addressList.postValue(it)
                                     },
                                     {
                                         Log.d(TAG, it.toString())
